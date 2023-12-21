10f344893c72559c15e4dfd9c21e346ac09178af
Luka Micheletti <michelettiluka@gmail.com>
2023-12-21 12:56:27 +0100

    Added write_docs_pls

--- lib/glyder_lints.dart
+++ lib/glyder_lints.dart
@@ -1,20 +1,19 @@
 import 'package:custom_lint_builder/custom_lint_builder.dart';
+
 import 'package:glyder_lints/assists/insert_spell_in_string_literal.dart';
-import 'package:glyder_lints/rules/amend_model_suffix.dart';
-import 'package:glyder_lints/rules/dont_say_his_name.dart';
 import 'package:glyder_lints/rules/specify_type.dart';
+import 'package:glyder_lints/rules/write_docs_pls.dart';
 
 // Plugin entry point.
 PluginBase createPlugin() => _GlyderLints();
 
-// The class listing all the [LintRule]s and [Assist]s defined by our plugin
+/// The class listing all the [LintRule]s and [Assist]s defined by our plugin
 class _GlyderLints extends PluginBase {
   // Lint rules.
   @override
   List<LintRule> getLintRules(CustomLintConfigs configs) => [
-        DontSayHisName(),
-        AmendModelSuffix(),
         SpecifyType(),
+        WriteDocsPls(),
       ];
 
   // Assists.
---
+++ lib/rules/write_docs_pls.dart
@@ -0,0 +1,47 @@
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/error/listener.dart';
+import 'package:custom_lint_builder/custom_lint_builder.dart';
+
+/// Enforces the use of documentation comments for classes and methods declarations.
+class WriteDocsPls extends DartLintRule {
+  static const LintCode _code = LintCode(
+    name: "write_docs_pls",
+    problemMessage: "Provide documentation comments for classes and methods",
+  );
+
+  WriteDocsPls() : super(code: _code);
+
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ErrorReporter reporter,
+    CustomLintContext context,
+  ) {
+    context.registry.addMethodDeclaration((MethodDeclaration node) {
+      final ExecutableElement? declaredMethod = node.declaredElement;
+
+      if (declaredMethod == null) {
+        return;
+      }
+
+      // Only report for public methods.
+      if (!declaredMethod.hasOverride && declaredMethod.isPublic && node.documentationComment == null) {
+        reporter.reportErrorForNode(code, node);
+      }
+    });
+
+    context.registry.addClassDeclaration((ClassDeclaration node) {
+      final ClassElement? declaredMethod = node.declaredElement;
+
+      if (declaredMethod == null) {
+        return;
+      }
+
+      // Only report for public classes.
+      if (declaredMethod.isPublic && node.documentationComment == null) {
+        reporter.reportErrorForNode(code, node);
+      }
+    });
+  }
+}
